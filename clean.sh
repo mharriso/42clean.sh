3276aad8d793b23821dfd088a9aade070b828e0c
Julia <63446729+mharriso@users.noreply.github.com>
2021-08-21 22:02:00 +0300

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,4 +1,3 @@
-
 RESET='\033[0m'
 RED='\033[31m'
 GREEN='\033[32m'
@@ -7,33 +6,24 @@ BLUE='\033[34;1m'
 MAGENTA='\033[35;1m'
 CYAN='\033[36m'
 
-# dirs for cleaning
 CACHES=~/Library/Caches/*
-VSCODE_CRASH=~/Library/ApplicationSupport/Code/Crashpad/completed/*
-VSODE_STORAGE=~/Library/ApplicationSupport/code/User/workspaceStorage/*
-VSCODE_CACHE=~/Library/ApplicationSupport/Code/CachedData/*
-SLACK_CACHE=~/Library/ApplicationSupport/Slack/Cache/*
-SLACK_CACHE1=/Users/mharriso/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
-CHROME=~/Library/ApplicationSupport/Google/Chrome/*
-DOCKER=/Users/mharriso/Library/Containers/*
 
 echo 'BEFORE'
 du -sh ~ 2> errors.txt
-
-rm -rf $CACHES
-rm -rf $VSCODE_CRASH
-rm -rf $VSCODE_STORAGE
-rm -rf $VSCODE_CACHE
-#rm -rf $CHROME
-rm -rf "${SLACK_CACHE1}"
-rm -rf $SLACK_CACHE
-#rm -rf $DOCKER
+rm -f errors.txt
+rm -rf ~/Library/Caches/*
+rm -rf ~/Library/Application\ Support/Slack/Cache/*
+rm -rf ~/Library/Application\ Support/Code/Crashpad/completed/*
+rm -rf ~/Library/Application\ Support/Code/CachedData/*
+rm -rf ~/Library/Application\ Support/code/User/workspaceStorage/*
 rm -rf ~/Library/42_cach
-rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/
-rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/
-
+rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
+rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/*
+rm -rf ~/Library/Keychains/*
+rm -rf ~/Library/Group\ Containers/*
 echo $GREEN'AFTER'
 du -sh ~ 2> ~errors.txt
+rm -f errors.txt
 echo $YELLOW'\n             *     ,MMM8&&&.            *
                   MMMM88&&&&&    .
                  MMMM88&&&&&&&
