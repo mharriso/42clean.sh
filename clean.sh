2a98f2f1d014fde25cefa0c9e1374bf7b77480a2
mharriso <mharriso@student.21-school.ru>
2022-07-04 17:22:54 +0300

    clean trash telegram chrome vscode

--- clean.sh
+++ clean.sh
@@ -5,12 +5,13 @@ YELLOW='\033[33m'
 BLUE='\033[34;1m'
 MAGENTA='\033[35;1m'
 CYAN='\033[36m'
-
-CACHES=~/Library/Caches/*
-
+``
 echo 'BEFORE'
 du -sh ~ 2> errors.txt
 rm -f errors.txt
+rm -rf ~/Library/Application\ Support/Telegram\ Desktop/tdata/user_data/*
+rm -rf ~/Library/Application\ Support/Google/Chrome/Default/Application\ Cache/*
+rm -rf ~/Library/Application\ Support/Code/CachedExtensionVSIXs/*
 rm -rf ~/Library/Caches/*
 rm -rf ~/Library/Application\ Support/Slack/Cache/*
 rm -rf ~/Library/Application\ Support/Code/Crashpad/completed/*
@@ -21,6 +22,7 @@ rm -rf ~/Library/Application\ Support/Slack/Service\ Worker/CacheStorage/*
 rm -rf ~/Library/Application\ Support/Slack/Code\ Cache/*
 rm -rf ~/Library/Keychains/*
 rm -rf ~/Library/Group\ Containers/*
+rm -rf .Trash
 echo $GREEN'AFTER'
 du -sh ~ 2> ~errors.txt
 rm -f errors.txt
